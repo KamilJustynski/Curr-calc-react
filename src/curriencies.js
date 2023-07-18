6aa9969ca6ed5da4c406eda51c55452d50612ddb
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-07-18 13:21:46 +0200

    adding currency table with rates

---
+++ src/curriencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+  {
+    short: "USD",
+    name: "Dolar Amerykański",
+    rate: 3.9451,
+  },
+  {
+    short: "EUR",
+    name: "Euro",
+    rate: 4.4331,
+  },
+  {
+    short: "GBP",
+    name: "Funt Brytyjski",
+    rate: 5.1596,
+  },
+  {
+    short: "CHF",
+    name: "Frank Szwajcarski",
+    rate: 4.5913,
+  },
+];
