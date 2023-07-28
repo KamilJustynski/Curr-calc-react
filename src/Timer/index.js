b346ba0208841bbb2aee5128a44d79a1cb56d225
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-07-28 16:44:21 +0200

    transfer formattedTime to separate component

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,8 +1,10 @@
 import { TimerContainer } from "./styled";
 import { useCurrentDate } from "../useCurrentDate";
+import { formattedTime } from "../formattedTime";
+
 
 const Timer = () => {
-  const { time, formattedTime } = useCurrentDate();
+  const { time } = useCurrentDate();
 
   return <TimerContainer>Dzisiaj jest {formattedTime(time)}</TimerContainer>;
 };
--- src/useCurrentDate.js
+++ src/useCurrentDate.js
@@ -13,15 +13,5 @@ export const useCurrentDate = () => {
     };
   }, []);
 
-  const formattedTime = () =>
-    time.toLocaleString(undefined, {
-      weekday: "long",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      day: "numeric",
-      month: "long",
-    });
-
-  return { useCurrentDate, formattedTime };
+  return { time };
 };
